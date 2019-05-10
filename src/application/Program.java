26eb9cbc0e67145ecebe6857e98c1e6a844385d6
Eliseu Macedo <eliseumcd@gmail.com>
2019-05-10 14:55:09 -0300

    Classe Departamento e Cliente implementada

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.text.SimpleDateFormat;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Department obj = new Department(1,	"Books");
+		
+		System.out.println(obj);
+		
+		
+
+
+	}
+
+}
